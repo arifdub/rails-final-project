8c14e98d5198348974fb282045f932753bc845c0
Muhammad Arif <arifdub@gmail.com>
2016-12-12 21:39:44 +0000

    order items table , order details

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122171622) do
+ActiveRecord::Schema.define(version: 20161207204850) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price"
+    t.string   "image_url"
+    t.string   "category"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "menus", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +32,28 @@ ActiveRecord::Schema.define(version: 20161122171622) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "orderitems", force: :cascade do |t|
+    t.integer  "item_id"
+    t.string   "title"
+    t.text     "description"
+    t.integer  "quantity"
+    t.decimal  "price"
+    t.integer  "order_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "orderitems", ["order_id"], name: "index_orderitems_on_order_id"
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "order_date"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
