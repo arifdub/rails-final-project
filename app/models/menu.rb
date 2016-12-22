7b08a605d433ce29723bb3c9f1ee032b52516131
Muhammad Arif <arifdub@gmail.com>
2016-12-22 16:36:27 +0000

    case

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,7 +1,7 @@
 class Menu < ActiveRecord::Base
 	def self.search(search)
 		if search
-		where("title like ? OR description like ?", "%#{search}%", "%#{search}%") 
+		where("title ilike ? OR description ilike ?", "%#{search}%", "%#{search}%") 
 		else
 			scoped
 		end
